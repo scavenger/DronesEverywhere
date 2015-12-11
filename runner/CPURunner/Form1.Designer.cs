353feae6599c884dcd6950c6d51852b9dd63c2a3
Cristian Mihailescu <Cristian.Mihailescu@sparkware.ro>
2015-12-11 18:05:57 +0200

    added runner

---
+++ runner/CPURunner/Form1.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace CPURunner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbConsole = new System.Windows.Forms.TextBox();
+            this.btnLoadIt = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbConsole
+            // 
+            this.tbConsole.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbConsole.Font = new System.Drawing.Font("Consolas", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbConsole.Location = new System.Drawing.Point(9, 12);
+            this.tbConsole.Multiline = true;
+            this.tbConsole.Name = "tbConsole";
+            this.tbConsole.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbConsole.Size = new System.Drawing.Size(938, 547);
+            this.tbConsole.TabIndex = 0;
+            // 
+            // btnLoadIt
+            // 
+            this.btnLoadIt.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnLoadIt.Location = new System.Drawing.Point(25, 570);
+            this.btnLoadIt.Name = "btnLoadIt";
+            this.btnLoadIt.Size = new System.Drawing.Size(75, 23);
+            this.btnLoadIt.TabIndex = 1;
+            this.btnLoadIt.Text = "Load";
+            this.btnLoadIt.UseVisualStyleBackColor = true;
+            this.btnLoadIt.Click += new System.EventHandler(this.btnLoadIt_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(959, 605);
+            this.Controls.Add(this.btnLoadIt);
+            this.Controls.Add(this.tbConsole);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbConsole;
+        private System.Windows.Forms.Button btnLoadIt;
+    }
+}
+
---
+++ runner/CPURunner/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CPURunner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ParseFile(string path)
+        {
+            var lines = File.ReadLines(path).ToArray();
+            var memory = Enumerable.Range(0, 5000).Select(x => 0).ToArray();
+            int A = 0, N = 0;
+            int lineCnt = 0;
+            var address = 0;
+
+            int completeRuns = 0;
+
+            var trackMemory = new[] {0, 1000, 1001, 1002, 3000, 4000, 3001, 4001, 3002, 4002, 3003, 4003, 3004, 4004, 3005, 4005, 3006, 4006};
+
+            while (true)
+            {
+                var res = lines[lineCnt].Split(new[] { ' ' }, StringSplitOptions.None);
+                var token = res[0];
+                var value = res.Length > 1 ? res[1] : "";
+                var hasAddress = false;
+                var skipIt = false;
+                int newLineCnt = lineCnt;
+
+                if (value.Contains("["))
+                {
+                    address = value.Contains("N") ? N : Convert.ToInt32(value.Substring(1, value.Length - 2));
+                    hasAddress = true;
+                }
+
+                switch (token)
+                {
+                    case "LDA":
+                        A = hasAddress ? memory[address] : Convert.ToInt32(value);
+                        break;
+                    case "JGE":
+                        if (A >= 0)
+                        {
+                            lineCnt = Convert.ToInt32(value);
+                        }
+                        skipIt = true;
+                        break;
+                    case "STA":
+                        if (hasAddress)
+                        {
+                            memory[address] = A;
+                        }
+                        break;
+                    case "LDN":
+                        N = hasAddress ? memory[address] : Convert.ToInt32(value);
+                        break;
+                    case "ADDA":
+                        A += Convert.ToInt32(value);
+                        break;
+                    case "SUBA":
+                        A -= Convert.ToInt32(value);
+                        break;
+                    case "HLT":
+                        lineCnt = 0;
+                        skipIt = true;
+                        completeRuns++;
+                        if (completeRuns > 8)
+                        {
+                            return;
+                        }
+                        tbConsole.Text += string.Format("{0}---------------------------------------{0}", Environment.NewLine);
+                        break;
+                    default:
+                        tbConsole.Text += string.Format("{0}: ERROR: {1}{2}", lineCnt, lines[lineCnt], Environment.NewLine);
+                        break;
+                }
+
+                var memoryMap = trackMemory.Aggregate("", (current, mapLocation) => current + string.Format("{0,4} ", memory[mapLocation]));
+                tbConsole.Text += string.Format("{0,5}: A = {1,5}, N = {2,5}, \t\t{3,15}\t\t{5}{4}", newLineCnt, A, N, lines[newLineCnt], Environment.NewLine, memoryMap);
+
+                if (!skipIt)
+                {
+                    lineCnt++;
+                    if (lineCnt == lines.Length)
+                    {
+                        lineCnt = 0;
+                    }
+                }
+            }
+        }
+
+        private void btnLoadIt_Click(object sender, EventArgs e)
+        {
+            this.ParseFile(@"C:\WORK\amazon2015\output\01_letsGetToKnowEachOther.txt");
+        }
+    }
+}
